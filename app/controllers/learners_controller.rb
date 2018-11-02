2367323717b982122aea2abdc3351196a2de5528
y.okada <ecopack516@gmail.com>
2018-11-02 11:55:19 +0900

    bundle update

--- app/controllers/learners_controller.rb
+++ app/controllers/learners_controller.rb
@@ -1,6 +1,6 @@
 class LearnersController < ApplicationController
   def index
-    @learner_all=User.where(position:0)
+    @learner_all=User.where.not(id:current_user.id).where(position:0)
   end
 
   def show
