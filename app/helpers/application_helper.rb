95bd37509a1acd6ed57e9429416a984dc1df5303
y.okada <ecopack516@gmail.com>
2018-12-19 10:23:59 +0900

    ApplicationHelperメソッド後置ifに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def position_check_return_all_seats_new(current_user_id)
     current_user = User.find(current_user_id)
-    if current_user.position == 1
-      link_to "座席数入力画面",new_all_seat_path
-    end
+      link_to "座席数入力画面",new_all_seat_path if current_user.position == 1
   end
 end
