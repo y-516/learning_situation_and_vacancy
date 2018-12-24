bb1b8b5a4d80c973c521141088e839d57c4cd621
y.okada <ecopack516@gmail.com>
2018-12-24 21:11:13 +0900

    modelリファクタリング スペース追加

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,9 +1,9 @@
 class Seat < ApplicationRecord
   belongs_to :user
-  validates :user_id,uniqueness:true
+  validates :user_id, uniqueness: true
   validate :learning_done_check
 
   def learning_done_check
-      errors.add(:learning_done,"は過去の時間を使用できません") if learning_done < DateTime.now
+      errors.add(:learning_done, "は過去の時間を使用できません") if learning_done < DateTime.now
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-  has_many:active_relationships,foreign_key:'follower_id',class_name:'Relationship'
-  has_many:passive_relationships,foreign_key:'followed_id',class_name:'Relationship'
-  has_many:following,through: :active_relationships,source: :followed
-  has_many:followers,through: :passive_relationships,source: :follower
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship'
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship'
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_many :seat
   mount_uploader :image, ImageUploader
-  validates :name,presence:true,length: { maximum: 30 }
-  validates :email ,length: { maximum: 255 }
-  validates :curriculum,length:{ maximum: 255 }
-  validates :learning_goal,length:{ maximum: 255 }
-  validates :ability,length:{ maximum: 255 }
-  validates :project,length:{ maximum: 255 }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, length: { maximum: 255 }
+  validates :curriculum, length: { maximum: 255 }
+  validates :learning_goal, length: { maximum: 255 }
+  validates :ability, length: { maximum: 255 }
+  validates :project, length: { maximum: 255 }
 
   def follow!(other_user)
     active_relationships.create!(followed_id: other_user.id)
